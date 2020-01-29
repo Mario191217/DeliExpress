3c166a9fc1db42cbae9dce4248d4f5674fbf209c
Mario Alvarenga <Mario Alvarenga@DESKTOP-B93F6A7>
2020-01-28 21:14:56 -0600

    Add Rols and gender

---
+++ DeliUsaid/DeliUsaid/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using DeliUsaid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeliUsaid.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        Contexto db = new Contexto();
+        public ActionResult Index()
+        {
+            if(db.Roles.Count() == 0)
+            {
+                AddRol("Representante");
+                AddRol("Consumidor");
+            }
+            if(db.Generos.Count() == 0)
+            {
+                AddGenero("Maculino");
+                AddGenero("Femenino");
+            }
+            return View();
+        }
+
+        public void AddRol(string rol)
+        {
+            Roles roles = new Roles();
+            roles.Rol = rol;
+            roles.Descripcion = rol;
+            db.Roles.Add(roles);
+            db.SaveChanges();
+        }
+
+        public void AddGenero(string genero)
+        {
+            Generos generos = new Generos();
+            generos.Genero = genero;
+            db.Generos.Add(generos);
+            db.SaveChanges();
+        }
+    }
+}
